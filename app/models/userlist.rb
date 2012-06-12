77cf1a1d553f7e7ccf054ad9d1becd479ecf0fa7
jpaulsingh <jatinder500@yahoo.com>
2012-06-12 16:51:19 -0500

    changes in table

--- app/models/userlist.rb
+++ app/models/userlist.rb
@@ -4,5 +4,4 @@ class Userlist < ActiveRecord::Base
    belongs_to :stock
    validates :user_id, :presence => true
    validates :stock_id, :presence => true
-#   validates :stock_id,  :uniqueness => true 
 end
